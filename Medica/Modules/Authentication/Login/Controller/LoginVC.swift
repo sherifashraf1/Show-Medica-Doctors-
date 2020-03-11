f12dd3f7b8f1791ca36bd9ac627e15107a49c983
sherifashraf910 <sherifashraf910@gmail.com>
2020-03-11 18:48:56 +0200

    Limit Phone Number to 11 number

--- Medica/Modules/Authentication/Login/Controller/LoginVC.swift
+++ Medica/Modules/Authentication/Login/Controller/LoginVC.swift
@@ -41,6 +41,7 @@ class LoginVC: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        phoneNumberTxtF.delegate = self
         phoneNumberTxtF.setLeftImage(imageName: "phone-icon")
         passwordTxtF.setLeftImage(imageName: "pass-icon")
         passBtn.setImage(UIImage(named: "eye"), for: .selected)
@@ -82,4 +83,14 @@ class LoginVC: UIViewController {
         present(vc, animated: true, completion: nil)
     }
 
+}
+
+// Limit Phone Number to 11 number
+extension LoginVC : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 11
+        let currentString = textField.text! as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string) as NSString
+        return newString.length <= maxLength
+    }
 }
--- Medica/Modules/Authentication/RegisterVC/Controller/RegisterVC.swift
+++ Medica/Modules/Authentication/RegisterVC/Controller/RegisterVC.swift
@@ -66,6 +66,7 @@ class RegisterVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        phoneNumberTxtF.delegate = self
         setRightImageOnTextFields()
         checkPassAndConfirmPassEqually()
         passBtn.setImage(UIImage(named: "eye"), for: .selected)
@@ -141,3 +142,12 @@ class RegisterVC: UIViewController {
     
 }
 
+// Limit Phone Number to 11 number
+extension RegisterVC : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 11
+        let currentString = textField.text! as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string) as NSString
+        return newString.length <= maxLength
+    }
+}
