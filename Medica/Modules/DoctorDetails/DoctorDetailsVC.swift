fceb9048937261caebc0755fc92f41cfcc21b94a
sherifashraf910 <sherifashraf910@gmail.com>
2020-03-11 04:53:19 +0200

    Decorate the Gesture Functions By combining them into extension of UIViewController , then add the dragUpView() and dragDownView() in doctorsDetails and AnnotDetails VCs

--- Medica/Modules/DoctorDetails/DoctorDetailsVC.swift
+++ Medica/Modules/DoctorDetails/DoctorDetailsVC.swift
@@ -32,48 +32,5 @@ class DoctorDetailsVC: UIViewController {
         doctorSpeciality.text = drSpeciality
         doctorPrice.text = drPrice
     }
-    
-    func dragUpView(){
-        let slideUp = UISwipeGestureRecognizer(target: self, action: #selector(dragViewFromTop(gesture:)))
-        slideUp.direction = .up
-        view.addGestureRecognizer(slideUp)
-    }
-    
-    func dragDownView(){
-        let slideDown = UISwipeGestureRecognizer(target: self, action: #selector(dragViewFromBottom(gesture:)))
-        slideDown.direction = .down
-        view.addGestureRecognizer(slideDown)
-    }
-    
-    @objc func dragViewFromBottom(gesture: UISwipeGestureRecognizer) {
-        if let window = UIApplication.shared.keyWindow { // keyWindow is the main window that receive an event , in this case it receive the gesture
-            gesture.view?.frame = CGRect(x:window.frame.width  , y: window.frame.height  , width: 5 , height: 5)
-            let transition: CATransition = CATransition()
-            transition.duration = 0.5
-            transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-            transition.type = CATransitionType.reveal
-            transition.subtype = CATransitionSubtype.fromBottom
-            self.view.window!.layer.add(transition, forKey: nil)
-            self.dismiss(animated: false,
-                         completion: {
-                            self.view.removeFromSuperview()
-            })
-        }
-    }
-    
-    @objc func dragViewFromTop(gesture: UISwipeGestureRecognizer) {
-        if let window = UIApplication.shared.keyWindow { // keyWindow is the main window that receive an event , in this case it receive the gesture
-            gesture.view?.frame = CGRect(x:window.frame.width  , y: window.frame.height  , width: 5 , height: 5)
-            let transition: CATransition = CATransition()
-            transition.duration = 0.5
-            transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-            transition.type = CATransitionType.reveal
-            transition.subtype = CATransitionSubtype.fromTop
-            self.view.window!.layer.add(transition, forKey: nil)
-            self.dismiss(animated: false,
-                         completion: {
-                            self.view.removeFromSuperview()
-            })
-        }
-    }
+  
 }
